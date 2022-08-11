c14d791255c27b0288a52fccdd33d1ca52acfaac
Juliano Pasa <julianodemellopasa@gmail.com>
2022-08-10 21:22:14 -0300

    CRC validation correction

--- crc.c
+++ crc.c
@@ -1,41 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "crc.h"
 
 #define POLYNOME 0x11021 // CRC-16 - x^16 + x^12 + x^5 + 1
-#define POLYNOME_DEGREE 16
 
+Crc crc(unsigned char *message, int size);
+
+unsigned char* copyMessage(unsigned char* message, int size);
 unsigned char* preProcessMessage(unsigned char* message, int size);
+
 void arrayXor(unsigned char* message);
 void arrayLeftShift(unsigned char* message, int size);
 
-Crc crc(unsigned char *message, int size)
+Crc generateCRC(unsigned char *message, int size)
 {
     unsigned char* dividend = preProcessMessage(message, size);
+    return crc(dividend, size);
+}
+
+Crc validateCRC(unsigned char *message, int size)
+{
+    unsigned char* dividend = copyMessage(message, size);
+    return crc(dividend, size);
+}
+
+Crc crc(unsigned char *message, int size)
+{
     Crc result;
-
-    printf("message: ");
-    for (int i = 0; i < size+2; i++)
-    {
-        printf("%u ", dividend[i]);
-    }
-    printf("\n");
-
 
     for (int division = 0; division < size*8; division++)
     {
-        if (dividend[0] & 0x80)
+        if (message[0] & 0x80)
         {
-            arrayLeftShift(dividend, size);
-            arrayXor(dividend);
+            arrayLeftShift(message, size);
+            arrayXor(message);
         }
         else
         {
-            arrayLeftShift(dividend, size);
+            arrayLeftShift(message, size);
         }
     }
 
-    result.digest[0] = dividend[0];
-    result.digest[1] = dividend[1];
+    result.digest[0] = message[0];
+    result.digest[1] = message[1];
 
     return result;
 }
@@ -52,6 +59,19 @@ unsigned char* preProcessMessage(unsigned char* message, int size)
 
     newMessage[i] = 0x0;
     newMessage[i+1] = 0x0;
+
+    return newMessage;
+}
+
+unsigned char* copyMessage(unsigned char* message, int size)
+{
+    unsigned char* newMessage = (unsigned char*) malloc(sizeof(unsigned char)*(size+2));
+    int i;
+
+    for (i = 0; i < size; i++)
+    {
+        newMessage[i] = message[i];
+    }
 
     return newMessage;
 }
@@ -77,11 +97,21 @@ void arrayXor(unsigned char* message)
 int main(int argc, char *argv[])
 {
     unsigned char message[] = {0x3B, 0xB6, 0xC2};
+    unsigned char response[5] = {0x3B, 0xB6, 0xC2};
 
-    Crc crcRedundancy = crc(message, sizeof(message));
+    Crc crcRedundancy = generateCRC(message, sizeof(message));
+    response[3] = crcRedundancy.digest[0];
+    response[4] = crcRedundancy.digest[1];
 
-    printf("\n");
+    Crc crcValidation = validateCRC(response, sizeof(response));
+
+    printf("%x %x %x \n", message[0], message[1], message[2]);
+
     printf("%x %x \n", crcRedundancy.digest[0], crcRedundancy.digest[1]);
+
+    printf("%x %x %x %x %x \n", response[0], response[1], response[2], response[3], response[4]);
+
+    printf("%x %x \n", crcValidation.digest[0], crcValidation.digest[1]);
 
     return 0;
 }
